9f8726c801190896a1cd2357ade7b4d61846dd26
linnieesz <86932868+linnieesz@users.noreply.github.com>
2023-08-14 20:52:51 -0300

    life system and game over

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 var altura = 0;
 var largura = 0;
+var vidas = 1;
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -18,6 +19,12 @@ function posicaoRandomica() {
     // remover mosquito anterior, caso exista
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove();
+
+        if (vidas > 3) {
+            window.location.href = 'fim_de_jogo.html'
+        }
+        document.getElementById('v' + vidas).src = './imagens/coracao_vazio.png'
+        vidas++
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90;
@@ -37,6 +44,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito);
 
